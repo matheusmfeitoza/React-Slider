f6ee35ba1b5298d6836a6a72ccda9e022e15260d
Matheus Marinho <feitozamatheus123@gmail.com>
2022-03-16 16:03:01 -0300

    Added initial logics to movement the slider

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -2,10 +2,30 @@ import React from "react";
 import styles from "./slider.module.css";
 
 const Slider = (props) => {
+  const [ativo, setAtivo] = React.useState(0);
+  const [position, setPosition] = React.useState(0);
+  const refPosition = React.useRef();
+
+  React.useEffect(() => {
+    const { width } = refPosition.current.getBoundingClientRect();
+    setPosition(ativo * width);
+  }, [ativo]);
+
+  function slidePrev() {
+    console.log("here");
+  }
+
+  function slideNext() {
+    console.log("here");
+  }
   return (
     <section>
       <div className={styles.container}>
-        <div className={styles.content}>
+        <div
+          className={styles.content}
+          style={{ transform: `translateX(${position}px)` }}
+          ref={refPosition}
+        >
           {props.sliders.map((slider) => (
             <div key={slider.id} className={styles.item}>
               <p>{slider.texto}</p>
@@ -13,8 +33,8 @@ const Slider = (props) => {
           ))}
         </div>
         <nav className={styles.button}>
-          <button>Prev</button>
-          <button>Next</button>
+          <button onClick={slidePrev}>Prev</button>
+          <button onClick={slideNext}>Next</button>
         </nav>
       </div>
     </section>
